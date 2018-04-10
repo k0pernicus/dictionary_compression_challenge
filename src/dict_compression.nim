f9a4eaac9848ed463c0f939c164cc0bb174309de
k0pernicus <antonin.carette@gmail.com>
2018-04-10 13:20:10 +0200

    Change indentation and improve code with Nim tips

--- src/dict_compression.nim
+++ src/dict_compression.nim
@@ -5,13 +5,13 @@ import strformat
 import strutils
 
 type 
-    Dictionary = seq[string]
+  Dictionary = seq[string]
 
 # comp -> Compress
 # uncomp -> Uncompress
 type
-    CompressionBehavior = enum
-        comp, uncomp
+  CompressionBehavior = enum
+    comp, uncomp
 
 const DICTIONARY_PATH: string = "/usr/share/dict/words"
 const COMPRESSED_FILE_PATH = "compressed_dictionary.txt"
@@ -19,40 +19,39 @@ const UNCOMPRESSED_FILE_PATH = "uncompressed_dictionary.txt"
 
 # TODO: Compress/Uncompress via ENUM
 proc actionOnDictionary(dictionary: Dictionary, feat: CompressionBehavior): Dictionary =
-    var rootWord = dictionary[0]
-    result = @[rootWord]
+  var rootWord = dictionary[0]
+  result = @[rootWord]
+  if feat == CompressionBehavior.comp:
     for word in dictionary[1..dictionary.len() - 1]:
-        var cWord: string
-        if feat == CompressionBehavior.comp:
-            cWord = compress(rootWord, word)
-            rootWord = word
-        else:
-            cWord = uncompress(rootWord, word)
-            rootWord = cWord
-        result.add(cWord)
+      result.add compress(rootWord, word)
+      rootWord = word
+  else:
+    for word in dictionary[1..dictionary.len() - 1]:
+      rootWord = uncompress(rootWord, word)
+      result.add rootWord
 
 proc actionOnDictionaryFromFile(pathfile: string, feat: CompressionBehavior) =
-    var ofpath = COMPRESSED_FILE_PATH
-    if feat == CompressionBehavior.uncomp:
-        ofpath = UNCOMPRESSED_FILE_PATH
-    var line: string = ""
-    var fs = newFileStream(pathfile, fmRead)
-    var dictionary: Dictionary = @[]
-    if not isNil(fs):
-        while fs.readLine(line):
-            dictionary.add(line.toLowerAscii())
-    let newDictionary = actionOnDictionary(dictionary, feat)
-    var ofs = newFileStream(ofpath, fmWrite)
-    if not isNil(ofs):
-        for cWord in newDictionary:
-            ofs.writeLine(cWord)
-    else:
-        echo fmt("Canno't open '{ofpath}' file")
+  var ofpath = COMPRESSED_FILE_PATH
+  if feat == CompressionBehavior.uncomp:
+    ofpath = UNCOMPRESSED_FILE_PATH
+  var line: string = ""
+  var fs = newFileStream(pathfile, fmRead)
+  var dictionary: Dictionary = @[]
+  if not isNil(fs):
+    while fs.readLine(line):
+      dictionary.add(line.toLowerAscii())
+  let newDictionary = actionOnDictionary(dictionary, feat)
+  var ofs = newFileStream(ofpath, fmWrite)
+  if not isNil(ofs):
+    for cWord in newDictionary:
+      ofs.writeLine(cWord)
+  else:
+    echo fmt("Canno't open '{ofpath}' file")
 
 when isMainModule:
-    echo fmt("> Compressing {DICTIONARY_PATH} in {COMPRESSED_FILE_PATH}...")
-    actionOnDictionaryFromFile(DICTIONARY_PATH, CompressionBehavior.comp)
-    echo "Done!"
-    echo fmt("> Uncompressing {COMPRESSED_FILE_PATH} in {UNCOMPRESSED_FILE_PATH}...")
-    actionOnDictionaryFromFile(COMPRESSED_FILE_PATH, CompressionBehavior.uncomp)
-    echo "Done!"
+  echo fmt("> Compressing {DICTIONARY_PATH} in {COMPRESSED_FILE_PATH}...")
+  actionOnDictionaryFromFile(DICTIONARY_PATH, CompressionBehavior.comp)
+  echo "Done!"
+  echo fmt("> Uncompressing {COMPRESSED_FILE_PATH} in {UNCOMPRESSED_FILE_PATH}...")
+  actionOnDictionaryFromFile(COMPRESSED_FILE_PATH, CompressionBehavior.uncomp)
+  echo "Done!"
--- tests/test.nim
+++ tests/test.nim
@@ -1,24 +1,24 @@
 import
-    unittest, lib.compress, lib.uncompress
+  unittest, lib.compress, lib.uncompress
 
 suite "my suite":
-    setup:
-        echo "suite setup"
-        var testVar = "from setup"
+  setup:
+    echo "suite setup"
+    var testVar = "from setup"
 
-    teardown:
-        echo "suite teardown"
+  teardown:
+    echo "suite teardown"
 
-    test "test compress":
-        check(compress("hola", "hello") == "1e1lo")
-        check(compress("hello", "hollo") == "1o3")
-        check(compress("commence", "commencer") == "8r")
-        check(compress("commencer", "commence") == "8")
-        check(compress("bonjour", "hola") == "h1la")
+  test "test compress":
+    check(compress("hola", "hello") == "1e1lo")
+    check(compress("hello", "hollo") == "1o3")
+    check(compress("commence", "commencer") == "8r")
+    check(compress("commencer", "commence") == "8")
+    check(compress("bonjour", "hola") == "h1la")
 
-    test "test uncompress":
-        check(uncompress("hola", "1e1lo") == "hello")
-        check(uncompress("hello", "1o3") == "hollo")
-        check(uncompress("commence", "8r") == "commencer")
-        check(uncompress("commencer", "8") == "commence")
-        check(uncompress("bonjour", "h1la") == "hola")
+  test "test uncompress":
+    check(uncompress("hola", "1e1lo") == "hello")
+    check(uncompress("hello", "1o3") == "hollo")
+    check(uncompress("commence", "8r") == "commencer")
+    check(uncompress("commencer", "8") == "commence")
+    check(uncompress("bonjour", "h1la") == "hola")
