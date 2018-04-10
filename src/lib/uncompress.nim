e8e7e326c5f5462794a6e117bc1a7a1e14cc3d13
k0pernicus <antonin.carette@gmail.com>
2018-04-10 13:23:15 +0200

    Add Nim idiom, using set and ord procedure

--- src/lib/uncompress.nim
+++ src/lib/uncompress.nim
@@ -6,8 +6,8 @@ proc uncompress*(previousWord, currentWord: string): string =
   result = ""
   var previousWordCopy = previousWord
   for c in currentWord:
-    let i = int(c) - int('0')
-    if i>0 and i<=9:
+    if c in {'1'..'9'}:
+      let i = ord(c) - ord('0')
       result.add(previousWordCopy[0..i-1])
       previousWordCopy = previousWordCopy[i..^1]
     else:
